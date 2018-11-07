60c8e2aae61aadd87d3e4f6a754a061de38d4265
srfrog <gus@dgraph.io>
2018-11-06 20:08:00 -0700

    Updating to release v1.0.10

--- Formula/dgraph.rb
+++ Formula/dgraph.rb
@@ -1,9 +1,9 @@
 class Dgraph < Formula
   desc "Fast, Distributed Graph DB"
   homepage "https://dgraph.io/"
-  url "https://github.com/dgraph-io/dgraph/releases/download/v1.0.9/dgraph-darwin-amd64.tar.gz"
-  version "1.0.9"
-  sha256 "9a3d518a4107a8c9bab2a71333bb7c80e7e875884c4cfd2ad5f30b500967d9eb"
+  url "https://github.com/dgraph-io/dgraph/releases/download/v1.0.10/dgraph-darwin-amd64.tar.gz"
+  version "1.0.10"
+  sha256 "1ca0eeca4a56d4ae6d102426ea04e412bc1fdad05ea56e2dd4979b434f67055e"
 
   def install
     bin.install "dgraph", "dgraph-ratel"
